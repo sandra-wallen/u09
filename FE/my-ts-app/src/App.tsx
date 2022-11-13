2c2f1f56d2b8fda4f63cb48387d42440bb8edc54
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-11-13 23:11:28 +0100

    set up router, configure redux store, add routes to login and profile

--- FE/my-ts-app/src/App.tsx
+++ FE/my-ts-app/src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+
 import './App.css';
+
+import Header from './components/header.component';
+import LandingPage from './pages/landing.page';
+import userSlice from './reducers/user.reducer';
+import Profile from './pages/profile.page';
+
+const reducer = combineReducers({
+  userSlice: userSlice.reducer,
+})
+
+export type rootState = ReturnType<typeof reducer>
+
+const store = configureStore({ reducer })
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Provider store={store}>
+            <Header />
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </Provider>
+        </BrowserRouter>
     </div>
   );
 }
