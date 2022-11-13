b9a71ac9d213406661ee2855fafb1b5a3e2b47b4
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-11-13 23:12:21 +0100

    add header and bootstrap navbar. Not dynamic yet

---
+++ FE/my-ts-app/src/components/header.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "./navbar.component";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  )
+}
+
+export default Header;
