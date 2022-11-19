bb9e330d0971841d097650e596383d0b017de63f
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-11-19 22:35:16 +0100

    Reusable axios function, needs clean up and refactoring

---
+++ FE/my-ts-app/src/reusable/useAxios.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+// Helt onödig. Det var useState som ställde till det. Ändra detta
+export function useAxios() {
+
+  return { callbackAxios };
+}
+
+function callbackAxios(method: string, url: string, data?: object) {
+
+  const res = axios({ 
+    method, 
+    url,
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+    data
+  })
+  .then((res) => {
+    console.log('res.data', res.data)
+    return res.data;
+  })
+  .catch((error) => {
+    console.log('error', error.response)
+    return error.response;
+  })
+
+  return res;
+}
