cef3d049be5d5769f59d84d8e7006fa28cd8c838
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-11-20 22:42:52 +0100

    Set up basic navigation to login if not auth in profile page

--- FE/my-ts-app/src/pages/profile.page.tsx
+++ FE/my-ts-app/src/pages/profile.page.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import schedulesSlice from "../reducers/schedule.reducer";
+import userSlice from "../reducers/user.reducer";
+import { persistor, resetStore, RootState } from "../store/store";
 
 
 const Profile: React.FC = () => {
+
+  const userState = useSelector((store: RootState) => store.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userState._id) {
+      navigate('/');
+    }
+  })
+
+
   return (
-    <></>
+    <main>
+    </main>
   )
 }
 
