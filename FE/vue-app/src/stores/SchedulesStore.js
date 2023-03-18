9c62b423fd0cf97525cfbb36f83b11837f35f2fb
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-03-19 00:45:22 +0100

    Set up functions to get and delete a schedule

--- FE/vue-app/src/stores/SchedulesStore.js
+++ FE/vue-app/src/stores/SchedulesStore.js
@@ -1,9 +1,15 @@
 import { defineStore } from "pinia";
 import { reactive, computed } from "vue";
+import { useNotification } from "@kyvg/vue3-notification";
+import { useCoursesStore } from "./CoursesStore";
 
 export const useSchedulesStore = defineStore("schedules", () => {
+	const { notify } = useNotification();
+	const coursesStore = useCoursesStore();
+
 	const model = reactive({
-		schedules: {},
+		schedules: [],
+		schedule: {},
 	});
 
 	const getSchedules = async () => {
@@ -15,17 +21,70 @@ export const useSchedulesStore = defineStore("schedules", () => {
 				},
 				credentials: "include",
 			});
-
-			console.log(response);
-
 			const data = await response.json();
-			console.log(data);
-
 			model.schedules = data.schedules;
 		} catch (error) {
-			console.log(error);
+			notify({
+				title: "Something went wrong",
+				text: "Your schedules couldn't be fetched. Please try reloading the page.",
+				type: "error",
+				duration: 10000,
+			});
 		}
 	};
 
-	return { model, getSchedules };
+	const deleteSchedule = async (id) => {
+		try {
+			const response = await fetch(
+				`http://localhost:8000/delete-schedule/${id}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					credentials: "include",
+				}
+			);
+			const data = await response.json();
+			if (data.success) {
+				getSchedules();
+			}
+		} catch (error) {
+			notify({
+				title: "Something went wrong",
+				text: "Your schedule couldn't be deleted. Please try again.",
+				type: "error",
+				duration: 10000,
+			});
+		}
+	};
+
+	const getSchedule = async (id) => {
+		try {
+			const response = await fetch(
+				`http://localhost:8000/schedule/${id}`,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					credentials: "include",
+				}
+			);
+
+			const data = await response.json();
+			if (data.success) {
+				model.schedule = data.schedule;
+			}
+		} catch (error) {
+			notify({
+				title: "Something went wrong",
+				text: "Couldn't find schedule. Please try again.",
+				type: "error",
+				duration: 10000,
+			});
+		}
+	};
+
+	return { model, getSchedules, deleteSchedule, getSchedule };
 });
