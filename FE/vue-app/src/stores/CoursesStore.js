c3486312a0961719dceb87edf5209e2ddcdf287d
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-03-19 00:46:10 +0100

    set up functions to get all courses associated to a schedule and delete a course associated to a schedule

---
+++ FE/vue-app/src/stores/CoursesStore.js
@@ -0,0 +1,68 @@
+import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
+import { useNotification } from "@kyvg/vue3-notification";
+import { useSchedulesStore } from "./SchedulesStore";
+
+export const useCoursesStore = defineStore("courses", () => {
+	const schedulesStore = useSchedulesStore();
+
+	const model = reactive({
+		courses: [],
+	});
+
+	const { notify } = useNotification();
+
+	const getCourses = async () => {
+		try {
+			const response = await fetch(`http://localhost:8000/courses`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+			});
+
+			const data = await response.json();
+			if (data.success) {
+				model.courses = data.courses;
+			}
+		} catch (error) {
+			notify({
+				title: "Something went wrong",
+				text: "Courses related to this schedule couln't be found. Please try reloading the page.",
+				type: "error",
+				duration: 10000,
+			});
+		}
+	};
+
+	const deleteCourseFromSchedule = async (courseId, scheduleId) => {
+		const filteredCourses = schedulesStore.model.schedule.courses.filter(
+			(item) => item.course !== courseId
+		);
+		try {
+			const response = await fetch(
+				`http://localhost:8000/update-schedule/${scheduleId}`,
+				{
+					method: "PATCH",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					credentials: "include",
+					body: JSON.stringify({ courses: filteredCourses }),
+				}
+			);
+
+			console.log(response);
+			const data = await response.json();
+			console.log(data);
+			if (data.success) {
+				schedulesStore.model.schedule = data.updatedSchedule;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return { model, getCourses, deleteCourseFromSchedule };
+});
