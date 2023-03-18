91fd2d9ab993fe7135ad1f1f78dd6eb5ba57d672
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-03-19 00:47:40 +0100

    App uses vue notifications package

--- FE/vue-app/src/main.js
+++ FE/vue-app/src/main.js
@@ -3,12 +3,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import Notifications from "@kyvg/vue3-notification";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(Notifications);
 app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
