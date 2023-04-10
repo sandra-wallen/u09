4b1f08b77b4febe487d5e560e305e5d5a27c27f3
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-04-10 16:56:52 +0200

    Style nav according to wireframe, make it dynamic

--- FE/vue-app/src/components/NavBar.vue
+++ FE/vue-app/src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg p-5 text-20">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand text-22 me-5" href="#">
                 App name
             </a>
+
+			<!--	Hamburger menu, small devices		-->
             <button
                 class="navbar-toggler"
                 type="button"
@@ -17,52 +19,60 @@
             </button>
             <div
                 class="collapse navbar-collapse"
-                id="navbarSupportedContent"
-            >
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                id="navbarSupportedContent">
+                <ul v-if="userStore.sessionExists" class="navbar-nav my-5 my-lg-0">
                     <li class="nav-item">
                         <RouterLink
                             to="/profile"
-                            class="nav-link">
+                            :class="['nav-link me-3', { 'text-underline fw-500': routeName === 'profile'}]">
                             Profile
                         </RouterLink>
                     </li>
                     <li class="nav-item">
                         <RouterLink
-                            to="/schedule-list"
-                            class="nav-link">
+                            to="/schedules"
+							:class="['nav-link me-3', { 'text-underline fw-500': routeName === 'schedules'}]">
                             My schedules
                         </RouterLink>
                     </li>
                     <li class="nav-item">
                         <RouterLink
-                            to="/"
-                            class="nav-link">
-                            Login
+                            to="/courses"
+							:class="['nav-link me-3', { 'text-underline fw-500': routeName === 'courses'}]">
+                            Courses
                         </RouterLink>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input
-                        class="form-control me-2"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                    />
-                    <button
-                        class="btn btn-outline-success"
-                        type="submit">
-                        Search
-                    </button>
-                </form>
-                <button
-                    class="btn btn-secondary mb-2 mb-lg-0 mx-2"
-                    type="button">
+                <button v-if="userStore.sessionExists"
+                    class="btn btn-secondary ms-auto"
+                    type="button"
+					@click="userStore.logoutUser()">
                     Logout
                 </button>
+				<button v-if="!userStore.sessionExists"
+					class="btn btn-primary ms-auto"
+					type="button"
+					@click="router.push({ path: '/login' })">
+					Login
+				</button>
             </div>
         </div>
     </nav>
 </template>
 <script setup>
+	import { computed, onUpdated } from "vue";
+	import { useUserStore} from "@/stores/UserStore";
+	import router from "@/router/index";
+	import { useRoute } from "vue-router";
+
+	const userStore = useUserStore();
+
+	const routeName = computed(() => useRoute().name)
 </script>
+<style lang="scss">
+	@import "@/styles/index";
+
+	.navbar {
+		background-color: $light-rose;
+	}
+</style>
