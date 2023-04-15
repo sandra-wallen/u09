2283819c33921f923ea07e8b697b40042911b022
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-04-15 22:23:28 +0200

    Styling and tweeking of how data is rendered

--- FE/vue-app/src/views/EditSchedulePage.vue
+++ FE/vue-app/src/views/EditSchedulePage.vue
@@ -1,14 +1,16 @@
 <template>
 	<main>
-		<h1>{{ schedulesStore.model.schedule.title }}</h1>
-		<form>
-			<div className="row gx-2 my-4">
-				<div className="col-12 col-md-6 p-2">
-					<div className="d-flex flex-column align-items-start mb-3">
+		<form class="d-flex flex-column justify-content-between">
+			<div class="row gx-2 my-4 schedule-wrapper justify-content-start">
+				<div class="col-12 mb-4">
+					<h1 class="text-start">{{ schedulesStore.model.schedule.title }}</h1>
+				</div>
+				<div class="col-12 col-md-5 p-2 pe-5">
+					<div class="d-flex flex-column align-items-start mb-3">
 						<label
 							for="inputTitle"
 							class="form-label">
-							Title
+							Schedule title
 						</label>
 						<input
 							type="text"
@@ -17,7 +19,7 @@
 							placeholder="Title"
 							v-model="schedulesStore.model.schedule.title"/>
 					</div>
-					<div className="d-flex flex-column align-items-start mb-3">
+					<div class="d-flex flex-column align-items-start mb-3">
 						<label
 							for="inputDuration"
 							class="form-label">
@@ -42,43 +44,50 @@
 						</select>
 					</div>
 				</div>
-				<div className="col-12 col-md-6 p-2">
-					<h2 className="text-start">Courses</h2>
-					<AddCourse />
-					<table>
-						<thead>
-							<tr>
-								<th scope="col">#</th>
-								<th scope="col">Title</th>
-								<th scope="col">
-									Start date & time
-								</th>
-								<th style="width: 20%"
-									scope="col">
-									Length
-								</th>
-								<th style="width: 20%"
-									scope="col">
-								</th>
+				<div class="col-12 col-md-7 p-2">
+					<div class="row gx-0">
+						<div class="col-6 d-flex justify-content-start align-items-end">
+							<h2 class="text-22">Courses</h2>
+						</div>
+						<div class="col-6 d-flex justify-content-end">
+							<AddCourse />
+						</div>
+						<table class="table container-sm">
+							<thead>
+							<tr class="text-start text-18">
+								<th style="width: 35%" scope="col">Title</th>
+								<th style="width: 20%" scope="col">Start date</th>
+								<th style="width: 20%" scope="col">Start time</th>
+								<th style="width: 15%" scope="col">Length</th>
+								<th style="width: 10%" scope="col"></th>
 							</tr>
-						</thead>
-						<tbody>
-							<tr v-for="(course, index) in courses" :key="course._id">
-								<th scope="row">{{index + 1}}</th>
+							</thead>
+							<tbody class="text-start text-18">
+							<tr v-for="(course) in courses" :key="course._id">
 								<td>
 									<RouterLink to="/">{{course?.title}}</RouterLink>
 								</td>
-								<td>{{ course?.startDateTime }}</td>
+								<td>{{ convertStartDate(course.startDateTime) }}</td>
+								<td>{{ convertStartTime(course.startDateTime) }}</td>
 								<td>{{ course?.length }}</td>
 								<td>
-									<button type="button" class="btn btn-danger" @click="handleDeleteCourse(course._id)">
-										Delete
+									<button type="button" class="delete-btn" @click="handleDeleteCourse(course._id)">
+										<font-awesome-icon icon="fa-regular fa-trash-alt" class="danger"/>
 									</button>
 								</td>
 							</tr>
-						</tbody>
-					</table>
+							</tbody>
+						</table>
+					</div>
 				</div>
+			</div>
+			<div class="d-flex justify-content-end align-items-end">
+				<button type="button" class="btn btn-secondary" @click="closeModal">
+					Back
+				</button>
+				<button type="button" class="btn btn-primary ms-3" @click="handleSubmit">
+					Save
+				</button>
 			</div>
 		</form>
 	</main>
@@ -90,6 +99,7 @@
 	import { useSchedulesStore } from "../stores/SchedulesStore";
 	import { useCoursesStore } from "../stores/CoursesStore"
 	import AddCourse from "@/components/AddCourse";
+	import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 	const courses = ref([])
 
@@ -109,6 +119,18 @@
 
 	const handleDeleteCourse = (courseId) => {
 		coursesStore.deleteCourseFromSchedule(courseId, id)
+	}
+
+	const convertStartDate = (date) => {
+		if (date) {
+			return new Date(date).toLocaleDateString()
+		}
+	}
+
+	const convertStartTime = (date) => {
+		if (date) {
+			return new Date(date).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+		}
 	}
 
 	const assignPropertiesToCourses = (arr) => {
@@ -138,3 +160,15 @@
 	})
 
 </script>
+<style scoped lang="scss">
+	@import "@/styles/index";
+
+	form {
+		min-height: 70vh;
+	}
+
+	.delete-btn {
+		border: none;
+		background: transparent;
+	}
+</style>
