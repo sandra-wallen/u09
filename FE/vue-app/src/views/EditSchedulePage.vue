c9c403e3ae74ce7628b46545d7ecd9dfb33f3458
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-04-20 23:23:40 +0200

    Update classes, update assignProperiesToCourses function to not alter the original values

--- FE/vue-app/src/views/EditSchedulePage.vue
+++ FE/vue-app/src/views/EditSchedulePage.vue
@@ -1,103 +1,103 @@
 <template>
-	<main class="d-flex flex-column justify-content-between">
-			<div class="row gx-2 my-4 schedule-wrapper justify-content-start">
-				<div class="col-12 mb-4">
-					<h1 class="text-start">{{ schedulesStore.model.schedule.title }}</h1>
-				</div>
-				<div class="col-12 col-md-5 p-2 pe-5">
-					<form>
-						<div class="d-flex flex-column align-items-start mb-3">
-							<label
-								for="inputTitle"
-								class="form-label">
-								Schedule title
-							</label>
-							<input
-								type="text"
-								id="inputTitle"
-								class="form-control"
-								placeholder="Title"
-								v-model="schedulesStore.model.schedule.title"/>
-						</div>
-						<div class="d-flex flex-column align-items-start mb-3">
-							<label
-								for="inputDuration"
-								class="form-label">
-								Duration
-							</label>
-							<select
-								id="inputDuration"
-								class="form-select"
-								aria-label="Input duration"
-								v-model="schedulesStore.model.schedule.duration">
-								<option disabled value="">
-									Duration in weeks
-								</option>
-								<option value="1">1</option>
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option value="5">5</option>
-								<option value="6">6</option>
-								<option value="7">7</option>
-								<option value="8">8</option>
-							</select>
-						</div>
-						<div class="d-flex justify-content-end">
-							<button type="button" class="btn btn-primary ms-3" @click="handleSubmit">
-								Save
-							</button>
-						</div>
-					</form>
-				</div>
-				<div class="col-12 col-md-7 p-2">
-					<div class="row gx-0">
-						<div class="col-6 d-flex justify-content-start align-items-end">
-							<h2 class="text-22">Courses</h2>
-						</div>
-						<div class="col-6 d-flex justify-content-end">
-							<AddCourse />
-						</div>
-						<table class="table container-sm">
-							<thead>
-							<tr class="text-start text-18">
-								<th style="width: 35%" scope="col">Title</th>
-								<th style="width: 20%" scope="col">Start date</th>
-								<th style="width: 20%" scope="col">Start time</th>
-								<th style="width: 15%" scope="col">Length</th>
-								<th style="width: 10%" scope="col"></th>
-							</tr>
-							</thead>
-							<tbody class="text-start text-18">
-							<tr v-for="(course) in courses" :key="course._id">
-								<td>
-									<RouterLink to="/">{{course?.title}}</RouterLink>
-								</td>
-								<td>{{ convertStartDate(course.startDateTime) }}</td>
-								<td>{{ convertStartTime(course.startDateTime) }}</td>
-								<td>{{ course?.length }}</td>
-								<td>
-									<button type="button" class="delete-btn" @click="handleDeleteCourse(course._id)">
-										<font-awesome-icon icon="fa-regular fa-trash-alt" class="danger"/>
-									</button>
-								</td>
-							</tr>
-							</tbody>
-						</table>
+	<main>
+		<div class="row gx-2 my-4 schedule-wrapper justify-content-start">
+			<div class="col-12 mb-4">
+				<h1 class="text-start">{{ schedulesStore.model.schedule.title }}</h1>
+			</div>
+			<div class="col-12 col-md-5 p-2 pe-5">
+				<form>
+					<div class="d-flex flex-column align-items-start mb-3">
+						<label
+							for="inputTitle"
+							class="form-label">
+							Schedule title
+						</label>
+						<input
+							type="text"
+							id="inputTitle"
+							class="form-control"
+							placeholder="Title"
+							v-model="schedulesStore.model.schedule.title"/>
 					</div>
+					<div class="d-flex flex-column align-items-start mb-3">
+						<label
+							for="inputDuration"
+							class="form-label">
+							Duration
+						</label>
+						<select
+							id="inputDuration"
+							class="form-select"
+							aria-label="Input duration"
+							v-model="schedulesStore.model.schedule.duration">
+							<option disabled value="">
+								Duration in weeks
+							</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+						</select>
+					</div>
+					<div class="d-flex justify-content-end">
+						<button type="button" class="btn btn-primary ms-3" :disabled="!formValid" @click="handleSubmit">
+							Save
+						</button>
+					</div>
+				</form>
+			</div>
+			<div class="col-12 col-md-7 p-2">
+				<div class="row gx-0">
+					<div class="col-6 d-flex justify-content-start align-items-end">
+						<h2 class="text-22">Courses</h2>
+					</div>
+					<div class="col-6 d-flex justify-content-end">
+						<AddCourse />
+					</div>
+					<table class="table container-sm">
+						<thead>
+						<tr class="text-start text-18">
+							<th style="width: 35%" scope="col">Title</th>
+							<th style="width: 20%" scope="col">Start date</th>
+							<th style="width: 20%" scope="col">Start time</th>
+							<th style="width: 15%" scope="col">Length</th>
+							<th style="width: 10%" scope="col"></th>
+						</tr>
+						</thead>
+						<tbody class="text-start text-18">
+						<tr v-for="(course) in courses" :key="course._id">
+							<td>
+								<RouterLink to="/">{{course?.title}}</RouterLink>
+							</td>
+							<td>{{ convertStartDate(course.startDateTime) }}</td>
+							<td>{{ convertStartTime(course.startDateTime) }}</td>
+							<td>{{ course?.length }}</td>
+							<td>
+								<button type="button" class="delete-btn" @click="handleDeleteCourse(course._id)">
+									<font-awesome-icon icon="fa-regular fa-trash-alt" class="danger"/>
+								</button>
+							</td>
+						</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
-			<div class="d-flex justify-content-start align-items-end">
-				<button type="button" class="btn btn-secondary" @click="goBack">
-					Back
-				</button>
-			</div>
+		</div>
+		<div class="d-flex justify-content-start align-items-end">
+			<button type="button" class="btn btn-secondary" @click="goBack">
+				Back
+			</button>
+		</div>
 
 	</main>
 </template>
 
 <script setup>
-	import { onMounted, ref, watch } from "vue";
+import {computed, onMounted, ref, watch} from "vue";
 	import { useRoute, useRouter } from "vue-router";
 	import { useSchedulesStore } from "../stores/SchedulesStore";
 	import { useCoursesStore } from "../stores/CoursesStore"
@@ -121,6 +121,11 @@
 		assignPropertiesToCourses(schedulesStore.model.schedule.courses)
 	})
 
+	const formValid = computed(() => {
+		return schedulesStore.model.schedule.title !== ""
+			&& schedulesStore.model.schedule.duration !== ""
+	})
+
 	const handleDeleteCourse = (courseId) => {
 		coursesStore.deleteCourseFromSchedule(courseId, id)
 	}
@@ -140,7 +145,8 @@
 	const assignPropertiesToCourses = (arr) => {
 		courses.value = schedulesStore.model.schedule.courses;
 
-		arr.forEach((item, index) => {
+		const newArr = arr
+		newArr.forEach((item, index) => {
 			coursesStore.model.courses.find(c => {
 				if (c._id === item.course) {
 					const courseProperties = {
@@ -159,7 +165,7 @@
 	}
 
 	const handleSubmit = () => {
-		if (schedulesStore.model.schedule.title !== "" && schedulesStore.model.schedule.duration) {
+		if (formValid.value) {
 			schedulesStore.updateSchedule(id, schedulesStore.model.schedule.title, schedulesStore.model.schedule.duration)
 		}
 	}
