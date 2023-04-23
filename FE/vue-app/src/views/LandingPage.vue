a4a112891d88436c3613f9b04f6b41cf67c821b4
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-04-23 19:48:20 +0200

    Add method to register, create register component and mount in landingpage

--- FE/vue-app/src/views/LandingPage.vue
+++ FE/vue-app/src/views/LandingPage.vue
@@ -1,14 +1,27 @@
 <template>
 	<main>
 		<LoginForm v-if="!register" />
+		<RegisterForm v-if="register" @created="toggleRegister" />
+		<div>
+			<p v-if="!register" class="text-16">Not a member yet?</p>
+			<p v-else class="text-16">Already a member?</p>
+			<button type="button" class="btn btn-secondary" @click="toggleRegister">
+				{{ !register ? "Register" : "Login" }}
+			</button>
+		</div>
 	</main>
 </template>
 
 <script setup>
-	import { ref } from "vue";
-	import LoginForm from "../components/LoginForm";
+	import { ref } from "vue"
+	import LoginForm from "../components/LoginForm"
+	import RegisterForm from "@/components/RegisterForm.vue"
 
-	const register = ref(false);
+	const register = ref(false)
+
+	const toggleRegister = () => {
+		register.value = !register.value
+	}
 
 </script>
 
