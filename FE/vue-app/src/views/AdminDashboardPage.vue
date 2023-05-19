527e6d6d8eaeee1c58ceec8ff9283861a11d8346
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2023-05-19 15:07:29 +0200

    getUsers() saves users in indexedDb when online and gets users from indexedDb when offline. Remove buttons and links that is used to create, update and delete when in offline mode

--- FE/vue-app/src/views/AdminDashboardPage.vue
+++ FE/vue-app/src/views/AdminDashboardPage.vue
@@ -1,7 +1,7 @@
 ﻿<template>
 	<main class="d-flex flex-column mb-3 px-5">
 
-		<CreateUser/>
+		<CreateUser v-if="!offline"/>
 		<h1 class="align-self-start">Users</h1>
 
 		<table v-if="filteredUsers.length > 0" class="table container-sm mt-3">
@@ -11,21 +11,23 @@
 				<th style="width: 25%" scope="col">Name</th>
 				<th style="width: 20%" scope="col" class="hide-on-mobile">Member since</th>
 				<th style="width: 10%" scope="col">Admin</th>
-				<th style="width: 15%" scope="col"></th>
+				<th v-if="!offline" style="width: 15%" scope="col"></th>
 			</tr>
 			</thead>
 			<tbody class="text-18">
 			<tr v-for="(user) in filteredUsers" class="text-start" :key="user._id">
 				<td>
-					<RouterLink
+					<RouterLink v-if="!offline"
 						class="me-3"
-						:to="`/admin-dashboard/update-user/${user._id}`">{{ user.email }}
+						:to="`/admin-dashboard/update-user/${user._id}`">
+						{{ user.email }}
 					</RouterLink>
+					<span v-else>{{ user.email }}</span>
 				</td>
 				<td>{{ user.name }}</td>
 				<td class="hide-on-mobile">{{ convertCreatedAt(user.createdAt) }}</td>
 				<td>{{ user.isAdmin ? "Yes" : "No" }}</td>
-				<td>
+				<td v-if="!offline">
 					<RouterLink
 						class="me-3"
 						:to="`/admin-dashboard/update-user/${user._id}`">
@@ -48,7 +50,7 @@
 </template>
 
 <script setup>
-	import { computed, onMounted } from "vue"
+	import { computed, onMounted, ref } from "vue"
 	import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 	import { useAdminStore } from "@/stores/AdminStore"
 	import { useUserStore } from "@/stores/UserStore"
@@ -58,6 +60,8 @@
 	const { notify } = useNotification()
 	const adminStore = useAdminStore()
 	const userStore = useUserStore()
+
+	const offline = ref(false)
 
 	onMounted(async () => {
 		const users = await adminStore.getUsers()
@@ -69,7 +73,15 @@
 				type: "error",
 				duration: 6000,
 			})
+		} else if (users.success && users.offline) {
+			notify({
+				title: "You are currently offline",
+				text: "Users have been loaded from cache, functionality is limited",
+				duration: 6000,
+			})
 		}
+
+		offline.value = users.offline
 	})
 
 	// Exclude logged in user from the list
